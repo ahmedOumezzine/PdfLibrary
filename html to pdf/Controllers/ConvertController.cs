76328847b661ef81e7acca08a3148c92e806dd87
ahmed Oumezzine <ahmedoumezzine@outlook.fr>
2019-05-18 17:12:50 +0100

    desgin

---
+++ html to pdf/Controllers/ConvertController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace html_to_pdf.Controllers
+{
+    public class ConvertController : Controller
+    {
+        // GET: Convert
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Html()
+        {
+            return View();
+        }
+        public ActionResult Link()
+        {
+            return View();
+        }
+        public ActionResult Invoice()
+        {
+            return View();
+        }
+        public ActionResult Ticket()
+        {
+            return View();
+        }
+    }
+}
